b17327467ad64c8b15462056f989e8475cd8fa6f
AlexBrandt46 <ajb3155@gmail.com>
2016-11-26 14:24:17 -0500

    Performs a factorial on any value of your choice.

---
+++ FactorialFinder.java
@@ -0,0 +1,27 @@
+package factorialfinder;
+
+import java.util.Scanner;
+
+public class FactorialFinder {
+    public static void main (String[] args) {
+        int initialValue;
+        int finalValue = 1;
+        Scanner in = new Scanner(System.in);
+        System.out.println ("What would you like to perform a factorial on?");
+            initialValue = in.nextInt();
+        System.out.println ("The value you'd like to take a factorial of is: " + initialValue + ".");
+        for (int i = initialValue; i >= 1; i--) {
+            finalValue = finalValue * i;
+        }
+        System.out.println ("The final value of " + initialValue + "when you perform a factorial on it is: " + finalValue);
+    }
+}
+
+
+/*this is the solution because if i is assigned the same value as initialValue
+and you decrement it after each time it'll multiply whatever the finalValue is by i
+and then assign the new value to finalValue, and if finalValue is initially assigned the value of 
+1 then it'll multiply itself by i
+*/
+
+//add to github
